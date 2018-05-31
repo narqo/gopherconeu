c5cdb8244f7c0d87d957ba6650b60fb89dbc25d8
Vladimir Varankin <vladimir@varank.in>
2018-05-31 11:18:06 +0000

    configure addr via envs

--- cmd/gophercon/main.go
+++ cmd/gophercon/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/narqo/gopherconeu/pkg/server"
 )
 
-const addr = ":8000"
-
 func main() {
+	addr := os.Getenv("ADDR")
+	if addr == "" {
+		log.Fatal("ADDR wasn't set")
+	}
+
 	r := server.New()
 
 	log.Printf("server is running: addr %s\n", addr)
