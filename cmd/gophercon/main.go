ef430fc9c027155ffafe8082f153777f5299f52b
Vladimir Varankin <vladimir@varank.in>
2018-05-31 10:23:40 +0000

    cmd: use router

--- cmd/gophercon/main.go
+++ cmd/gophercon/main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const addr = ":8000"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, GopherCon EU")
-	})
+	r := mux.NewRouter()
+
+	r.Handle("/home", homeHandler()).Methods(http.MethodGet)
 
 	log.Printf("server is running: addr %s\n", addr)
 
-	http.ListenAndServe(addr, nil)
+	http.ListenAndServe(addr, r)
+}
+
+func homeHandler() http.Handler {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, GopherCon EU")
+	}
+	return http.HandlerFunc(h)
 }
