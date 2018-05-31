22b3c36f5c912f0436cc792fa63f6b4957a3e5b9
Vladimir Varankin <vladimir@varank.in>
2018-05-31 10:59:15 +0000

    move things around
    because reasons

--- cmd/gophercon/main.go
+++ cmd/gophercon/main.go
@@ -4,16 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/narqo/gopherconeu/pkg/server"
 )
 
 const addr = ":8000"
 
 func main() {
-	r := mux.NewRouter()
-
-	r.Handle("/home", server.HomeHandler()).Methods(http.MethodGet)
+	r := server.New()
 
 	log.Printf("server is running: addr %s\n", addr)
 
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,9 +3,19 @@ package server
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func HomeHandler() http.Handler {
+func New() http.Handler {
+	r := mux.NewRouter()
+
+	r.Handle("/home", homeHandler()).Methods(http.MethodGet)
+
+	return r
+}
+
+func homeHandler() http.Handler {
 	h := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, GopherCon EU")
 	}
